f5bec185a6ff9ef4b7cec2c1ef4bdaf653e8b6b7
danielazeveedo2k12 <danielazeveedo2k12@gmail.com>
2020-11-01 19:15:06 -0300

    add middleware routes fuel

--- src/middlewares/valid/fuel.request.js
+++ src/middlewares/valid/fuel.request.js
@@ -7,10 +7,17 @@ const i18n = require("i18n")
 exports.verify = [
     body('name')
     .exists().withMessage(i18n.__('validate.exists', {name: 'nome'}))
-    .notEmpty().withMessage(i18n.__('validate.not-empty', {name: 'nome'}))
+    .notEmpty().withMessage(i18n.__('validate.not-empty', {name: 'descrição'}))
     .isAlphanumeric().withMessage(i18n.__('validate.alpha-numeric', {name: 'nome'}))
     .isLength({min: 1 , max: 50}).withMessage(i18n.__('validate.length', {name: 'nome'}))
     .trim(),
+
+    body('description')
+    .exists().withMessage(i18n.__('validate.exists', {name: 'descrição'}))
+    .notEmpty().withMessage(i18n.__('validate.not-empty', {name: 'descrição'}))
+    .isAlphanumeric().withMessage(i18n.__('validate.alpha-numeric', {name: 'descrição'}))
+    .trim(),
+
     (req,res,next) => { 
         let errorValidation = validationResult(req)
         if ( errorValidation.errors.length > 0 ) {
--- src/routes/fuel.js
+++ src/routes/fuel.js
@@ -3,10 +3,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('~control/fuel.controller');
+const valid = require('~/middlewares/valid/fuel.request')
 
 router.get('/:id?', controller.get );
-router.post('/', controller.create );
-router.put('/:id', controller.update );
+router.post('/', valid.verify, controller.create );
+router.put('/:id', valid.verify, controller.update );
 router.delete('/:id', controller.delete );
 
 module.exports = router;
